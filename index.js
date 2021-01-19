c6c291b9dc71701ec08f53bbd43ae7cd2116e95f
A.J. Romaniello <ajiellodev@gmail.com>
2021-01-19 03:08:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,97 +1,66 @@
-let firstNode = {name: 'susie', next: 'rkjasj'}
-let secondNode = {name: 'sam', next: 'asnan'}
-let newNode = {name: 'jill', next: ''}
-let lastNode = {name: 'charlie', next: null}
-let collection = {rkjasj: secondNode,
-  asnan: lastNode,
-  whana: firstNode,
-  ajhsak: newNode}
-  let linkedList = 'whana'
 
-function next(head, col) {
-  return col[head["next"]]
+function getName(node){
+return node.name;
 }
 
-function getName(node) {
-  return node["name"]
+function next(node, collection){
+let nextAddress = node.next
+return collection[`${nextAddress}`]
 }
 
-function headNode(link, col) {
-  return col[link];
+function addressAt(index, linkedList, collection){
+if(index == 0){
+  return linkedList
+} else {
+  let node = nodeAt(index-1, linkedList, collection)
+  return node.next
+}
 }
 
-function nodeAt(idx, link, col) {
-  if (idx == 0) return headNode(link, col);
-  let i = 1;
-  let node = headNode(link, col);
-
-  while (i <= idx) {
-    node = next(node, col);
-    i++;
-  }
-
-  return node;
+function headNode(linkedList, collection){
+return collection[linkedList]
 }
 
-function addressAt(idx, link, col) {
-  if (idx == 0 ) return link;
-
-  return nodeAt(idx - 1, link, col)['next']
+function nodeAt(index, linkedList, collection){
+let currentNode = headNode(linkedList, collection);
+for(let i = 0; i < index; i++){
+   currentNode = next(currentNode, collection);
 }
 
-function indexAt(node, col, link) {
-  let hNode = headNode(link, col)
-  let cachedNode = hNode;
-  let i = 0;
-  for (i; i < 10; i++) {
-    if (cachedNode == node) {
-      return i;
-    }
-
-    cachedNode = next(hNode, col)
-  }
-
-  return i;
+return currentNode;
 }
 
-function insertNodeAt(idx, addressTo, link, col) {
-  let addy = addressAt(idx - 1, link, col);
-  let node = col[link];
-  let newPath, prev;
-  while (idx > 0) {
-    prev = node['next']
-    node = next(node, col);
-    newPath = node['next']
-    idx--;
-  }
-
-  let previous = col[prev];
-  previous['next'] = newPath;
-
-  let lin = col[addressTo];
-  lin['next'] = prev;
-
-  let fin = col[addy];
-  fin['next'] = addressTo
-
-  return node;
+function indexAt(node, collection, linkedList){
+let currentNode = headNode(linkedList, collection);
+let currentIdx = 0
+while(currentNode != node){
+  currentIdx++
+  currentNode = next(currentNode, collection)
+}
+return currentIdx
 }
 
-function deleteNodeAt(idx, list, col) {
-  let header = col[list];
-  let node = header;
-  let deleteMe, newPath;
-  while (idx >= 0) {
-    if (idx == 1) {
-      deleteMe = node['next']
-      idx--;
-    }
 
-    node = next(node, col)
-    newPath = node["next"];
-    idx--;
-  }
+function insertNodeAt(index, newNodeAddress, linkedList, collection){
+let previousNode = nodeAt(index - 1, linkedList, collection)
+let subsequentNode = nodeAt(index, linkedList, collection)
 
-  col[deleteMe] = null
-  header['next'] = newPath
+let previousNodeIdx = indexAt(previousNode, collection, linkedList)
+let subsequentNodeIdx = indexAt(subsequentNode, collection, linkedList)
+let previousNodeAddress = addressAt(previousNode, linkedList, collection)
+let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
+previousNode.next = newNodeAddress
+let newNode = collection[newNodeAddress]
+newNode.next = subsequentNodeAddress
+}
+
+
+function deleteNodeAt(index, linkedList, collection){
+let previousNode;
+let currentNode = headNode(linkedList, collection);
+for(let i = 0; i < index; i++){
+   previousNode = currentNode
+   currentNode = next(currentNode, collection);
+}
+previousNode.next = currentNode.next
 }
